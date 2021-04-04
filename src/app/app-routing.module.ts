8a9e3538883e36b61c4e3975286b1d84bfe42a77
diegoacarvalhop <diegoacarvalhop@hotmail.com>
2021-04-04 20:48:08 -0300

    DELETE implementado para Livros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CategoriaUpdateComponent } from './components/views/categoria/categoria
 import { CetegoriaDeleteComponent } from './components/views/categoria/cetegoria-delete/cetegoria-delete.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
+import { LivroDeleteComponent } from './components/views/livro/livro-delete/livro-delete.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: 'categorias/:id_cat/livros/:id/update',
     component: LivroUpdateComponent
+  },
+  {
+    path: 'categorias/:id_cat/livros/:id/delete',
+    component: LivroDeleteComponent
   }
 ];
 
